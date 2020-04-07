1f0608bda4ad0a4c7d3d2563091e0374c401be60
EdixonAlberto <edixonalbertto@gmail.com>
2020-04-07 13:29:42 -0400

    changes
    - showing logo "fazt.png" from /assets/img
    - homepage empty
    - modifying readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # music-player-frontend
+
+<!-- TODO: Add description -->
+
+### Tecnologies
+
+- Enviroment: `JavaScript ES6`
+- Frameword: `Reactjs`
+- Request: `axiosjs`
+
+### Start
+
+```sh
+$ npm install
+
+$ npm start
+```
--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/img/fazt.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header avatar">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
