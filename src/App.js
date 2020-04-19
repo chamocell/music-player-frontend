2ce9ba3c31f8e97554894d0c067524b1af4ba122
zd4y <ariasorozco03@gmail.com>
2020-04-18 21:42:47 -0500

    Add react router and layout components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 // Components
 import Button from './components/Button';
+import Home from './components/Home';
+import Main from './components/Main';
+import Sidebar from './components/Sidebar';
 
 function App() {
-  // This is just an example
-  return <Button />;
+  return (
+    <Router>
+      <Sidebar title="App Name"></Sidebar>
+      <Main>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route>
+            <p>Not found</p>
+          </Route>
+        </Switch>
+      </Main>
+    </Router>
+  );
 }
 
 export default App;
