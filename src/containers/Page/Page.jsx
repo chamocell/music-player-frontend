c73d7547f10f73b07961c3c46bd9a17b31e653b9
Fernando Lopez <lopezfer@globalhitss.com>
2020-06-13 19:42:36 -0300

    move folders

--- src/containers/Page/Page.jsx
+++ src/containers/Page/Page.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import MusicPlayer from '../../components/MusicPlayer';
+import MusicPlayer from '../MusicPlayer';
 import Main from './../Main';
 import Sidebar from '../../components/Sidebar';
 
---
+++ src/theme.js
@@ -0,0 +1,44 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			//light: '#757ce8',
+			main: '#47536b',
+			//dark: '#002884',
+			//contrastText: '#fff',
+		},
+		secondary: {
+			//light: '#ff7961',
+			main: '#e1251b',
+			//dark: '#ba000d',
+			//contrastText: '#000',
+		},
+		grayColor: {
+			main: '#212224'
+		},
+		fontColorPrimary: {
+			main: '#99CBFF'
+		},
+		colorActive: {
+			main: '#47536b'
+		}
+	},
+	shadowBox: {
+		generic: '0 3px 5px 2px rgba(0, 0, 0, .4)'
+	},
+	typography: {
+		//fontFamily: ['Roboto'].join(','),
+		subtitle1: {
+			fontSize: 12,
+		},
+		body1: {
+			fontWeight: 500,
+		},
+		button: {
+			fontStyle: 'italic',
+		},
+	},
+});
+
+export default theme;
