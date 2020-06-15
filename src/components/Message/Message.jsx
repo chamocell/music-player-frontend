2ae650fc76ae9f8ac461ec17970c44ab49d7b38b
Fernando Lopez <lopezfer@globalhitss.com>
2020-06-15 20:49:43 -0300

    fix message error and eslint

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,31 +1,11 @@
-import React  from 'react'
+import React from 'react';
 
 // Material Components
-import { withStyles } from "@material-ui/core/styles"
-import Grid from "@material-ui/core/Grid"
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
-const Message = ({ classes, children, height, status}) => {
-
-  const image = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSLpQJ50USYZDEUyOESNxZwrsDaJ5h8Qa04oxQMU1fKQmw4wD5-&usqp=CAU';
-  return (
-    <Grid container spacing={0} className={`${classes.globalError}`}>
-        <Grid item xs={4} className={classes.contentError} style={{ height: height }}>
-            <img src={image} alt="popcorn icon" className={classes.image} />
-            <p style={{ display: 'flex', flexDirection: 'column' }}>
-                <strong style={{ fontSize: 30 }}>
-                 {status ? status : '404'}
-                </strong>
-                <span style={{ color: "#a5aab1", fontSize: 23, marginTop: 5, fontWeight: 300 }}>
-                {children ? children : 'No encontrado'}
-                </span>
-            </p>
-        </Grid>
-    </Grid>
-  )
-};
-
-const styles = () => ({
-	globalError: {
+const useStyles = makeStyles((theme) => ({
+  globalError: {
     width: '100%',
     height: '100%',
     display: 'flex',
@@ -42,6 +22,39 @@ const styles = () => ({
   image: {
     height: 140,
     marginBottom: 15
+  },
+  children: {
+    color: '#a5aab1',
+    fontSize: 23,
+    marginTop: 5,
+    fontWeight: 300
+  },
+  row: { 
+    display: 'flex', 
+    flexDirection: 'column' 
+  },
+  notFound: { 
+    fontSize: 30 
   }
-});
-export default withStyles(styles)(Message);
+}));
+
+const Message = ({ children, height, status }) => {
+  const classes = useStyles({ children, height, status });
+  const image =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSLpQJ50USYZDEUyOESNxZwrsDaJ5h8Qa04oxQMU1fKQmw4wD5-&usqp=CAU';
+  return (
+    <Grid container spacing={0} className={`${classes.globalError}`}>
+      <Grid item xs={4} className={classes.contentError}>
+        <img src={image} alt="popcorn icon" className={classes.image} />
+        <p className={classes.row}>
+          <strong className={classes.notFound}>{status ? status : '404'}</strong>
+          <span className={classes.children}>
+            {children ? children : 'No encontrado'}
+          </span>
+        </p>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Message;
