2c7a099fce99430061070c5e7ee349e743072252
Fernando Lopez <lopezfer@globalhitss.com>
2020-06-20 15:41:23 -0300

    add moment

--- src/components/AlbumSong/AlbumSong.jsx
+++ src/components/AlbumSong/AlbumSong.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import moment from 'moment';
 
 export default function AlbumSong({ song, index, className }) {
   return (
     <li className={'album-song ' + className}>
       <div className="album-song-index">{index}</div>
       <div className="album-song-title">{song.title}</div>
-      <div className="album-song-length">{song.length}</div>
+      <div className="album-song-length">
+        {index === '#'
+          ? song.length
+          : moment({ minute: 0, seconds: 0 }).add(song.length, 's').format('mm:ss')}
+      </div>
     </li>
   );
 }
